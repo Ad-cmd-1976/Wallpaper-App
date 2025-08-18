5a08d25325bfded230637ddff61d1f1fcdf5013b
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-08-18 15:29:59 +0530

    Vercel Connect to Render test

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api":"/api",
+    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api":"https://wallpaper-app-1.onrender.com",
     withCredentials:true
 });
 
