7d43779c05ad85dc08718bfb1ab9cc6cc5d3cc77
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2026-01-04 19:04:10 +0530

    Fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api":"https://wallpaper-app-1.onrender.com/api",
+    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api": import.meta.env.API_BASE_URL,
     withCredentials:true
 });
 
