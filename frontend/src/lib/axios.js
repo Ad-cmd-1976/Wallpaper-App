2c7360e0c9a8f134a6ac96e3f2457267daae3b20
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2026-01-04 19:10:37 +0530

    Deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api": import.meta.env.API_BASE_URL,
+    baseURL:import.meta.env.MODE==="development" ? "http://localhost:8080/api": import.meta.env.VITE_API_BASE_URL,
     withCredentials:true
 });
 
